2a775470a9a17c7d7e3b5c00effcafd20ef41cec
Jorge <jorge.navarro@webbeds.com>
2023-09-15 17:05:55 +0200

    api test py correctly set up and saving data in local postgres

--- api_test.py
+++ api_test.py
@@ -4,13 +4,53 @@
 
 import numpy as np
 import pandas as pd
-import requests as rq
+import requests
+import psycopg2
+import json
 
-# enter api credentials
+from sqlalchemy import create_engine
+
+# Define la ruta completa al archivo de configuración
+ruta_al_archivo = 'C:/Users/JorgeNavarro/Documents/projects/config/config.json'
+
+# Lee las credenciales desde el archivo de configuración
+with open(ruta_al_archivo, 'r') as config_file:
+    config = json.load(config_file)
+
+# Acceder a las credenciales
+db_host = config['postgres']['host']
+db_database = config['postgres']['database']
+db_user = config['postgres']['user']
+db_password = config['postgres']['password']
+
 
 # get data from api
-url = 'https://api.census.gov/data/2019/pep/population?get=POP,GEONAME&for=state:*&DATE_CODE=12'
-response = rq.get(url)
-data = rq.json()
+url = 'http://universities.hipolabs.com/search?country=United+States'
+response = requests.get(url)
+data = response.json()
 
-print(data)
+# convert data to pandas dataframe
+df = pd.DataFrame(data)
+print(df.head())
+
+
+# Especifica los parámetros de conexión, incluyendo el esquema deseado
+connection_params = {
+    "host": db_host,
+    "database": db_database,
+    "user": db_user,
+    "password": db_password,
+    "options": f"-c search_path=test"
+}
+
+# Establece la conexión
+conn = psycopg2.connect(**connection_params)
+
+# Realiza operaciones en la base de datos aquí
+engine = create_engine(
+    f"postgresql+psycopg2://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}/{connection_params['database']}"
+)
+df.to_sql('usa_universities', engine, if_exists='replace', index=False)
+
+# Cierra la conexión cuando hayas terminado
+conn.close()
